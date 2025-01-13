6dfebb63f0c5ac11ffa55ae7b31cb9a1346b5dcf
NoahJCross <s223226235@deakin.edu.au>
2025-01-14 02:45:47 +1100

    Updated pascal test printing

--- generated_tests/pascal/test_main.pas
+++ generated_tests/pascal/test_main.pas
@@ -31,16 +31,189 @@ uses
   testreport,
   DigestTestReport;
 
+{ Create a custom results writer }
+type
+  TCustomTestWriter = class(TTestCase, IInterface, ITestListener)
+  private
+    { IInterface }
+    FCurrentTest: TTest;  
+    FTestFailed: Boolean; 
+    function QueryInterface(constref IID: TGUID; out Obj): HResult; cdecl;
+    function _AddRef: Integer; cdecl;
+    function _Release: Integer; cdecl;
+  public
+    procedure WriteHeader; virtual;
+    procedure WriteResult(aResult: TTestResult); virtual;
+    { ITestListener }
+    procedure AddFailure(ATest: TTest; AFailure: TTestFailure); virtual;
+    procedure AddError(ATest: TTest; AError: TTestFailure); virtual;
+    procedure StartTest(ATest: TTest); virtual;
+    procedure EndTest(ATest: TTest); virtual;
+    procedure StartTestSuite(ATestSuite: TTestSuite); virtual;
+    procedure EndTestSuite(ATestSuite: TTestSuite); virtual;
+  end;
+
 { Variable declarations section }
 var
-  FXMLResultsWriter: TXMLResultsWriter;
-  FDigestResultsWriter: TDigestResultsWriter;
   testResult: TTestResult;
   groupName: string;
   testName: string;
   i: Integer;
   testSuite: TTestSuite;
   foundTest: TTest;
+  FCustomWriter: TCustomTestWriter;
+
+{ Implementation of IInterface methods }
+function TCustomTestWriter.QueryInterface(constref IID: TGUID; out Obj): HResult; cdecl;
+begin
+  if GetInterface(IID, Obj) then
+    Result := S_OK
+  else
+    Result := E_NOINTERFACE;
+end;
+
+function TCustomTestWriter._AddRef: Integer; cdecl;
+begin
+  Result := -1; // No reference counting needed
+end;
+
+function TCustomTestWriter._Release: Integer; cdecl;
+begin
+  Result := -1; // No reference counting needed
+end;
+
+{ Implementation of custom writer methods }
+procedure TCustomTestWriter.WriteHeader;
+begin
+  // Optional: Add any header text you want
+end;
+
+procedure TCustomTestWriter.WriteResult(aResult: TTestResult);
+var
+    i: Integer;
+    failure: TTestFailure;
+begin
+    writeln;
+    writeln('Test Run Summary');
+    writeln('----------------');
+    writeln('Total Tests Run: ', aResult.RunTests);
+    writeln('Failed Tests:    ', aResult.NumberOfFailures);
+    writeln('Errors:          ', aResult.NumberOfErrors);
+    
+    if (aResult.NumberOfFailures > 0) or (aResult.NumberOfErrors > 0) then
+    begin
+        writeln;
+        writeln('Failed Tests Details:');
+        writeln('--------------------');
+        
+        if aResult.NumberOfFailures > 0 then
+        begin
+            writeln('Failures:');
+            for i := 0 to aResult.Failures.Count - 1 do
+            begin
+                failure := TTestFailure(aResult.Failures[i]);
+                if failure.ExceptionMessage <> '' then
+                    writeln('Failure: ', failure.ExceptionMessage)
+                else if failure.AsString <> '' then
+                    writeln('Failure: ', failure.AsString)
+                else
+                    writeln('Failure: <No message available>');
+                writeln('   Message: ', failure.ExceptionClassName);                
+                if failure.SourceUnitName <> '' then
+                    writeln('   Source Unit: ', failure.SourceUnitName);
+                    
+                writeln;
+            end;
+        end;
+        
+        if aResult.NumberOfErrors > 0 then
+        begin
+            writeln('Errors:');
+            for i := 0 to aResult.Errors.Count - 1 do
+            begin
+                failure := TTestFailure(aResult.Errors[i]);
+                writeln('ERROR: ', failure.ExceptionClassName);
+                writeln('  Message: ', failure.ExceptionMessage);
+                
+                if failure.SourceUnitName <> '' then
+                    writeln('  Unit: ', failure.SourceUnitName);
+                if failure.FailedMethodName <> '' then
+                    writeln('  Method: ', failure.FailedMethodName);
+                if failure.LineNumber <> 0 then
+                    writeln('  Line: ', failure.LineNumber);
+                    
+                writeln;
+            end;
+        end;
+    end;
+end;
+
+procedure TCustomTestWriter.AddFailure(ATest: TTest; AFailure: TTestFailure);
+var
+  msg: string;
+  locationParts: TStringArray;
+  lineStr: string;
+begin
+  FTestFailed := True;
+  msg := AFailure.ExceptionMessage;
+  
+  AFailure.SourceUnitName := ATest.TestSuiteName;
+  AFailure.FailedMethodName := ATest.TestName;
+  
+  if AFailure.LocationInfo <> '' then
+  begin
+    locationParts := AFailure.LocationInfo.Split('(');
+    if Length(locationParts) > 1 then
+    begin
+      lineStr := locationParts[1].Split(')')[0];
+      AFailure.LineNumber := StrToIntDef(lineStr, 0);
+    end;
+  end;
+
+  writeln('FAILED: ', ATest.TestSuiteName, '.', ATest.TestName);
+  if msg <> '' then
+    writeln('  Message: ', msg)
+  else
+    writeln('  Message: Test assertion failed (no message provided)');
+  writeln;
+end;
+
+procedure TCustomTestWriter.AddError(ATest: TTest; AError: TTestFailure);
+begin
+  writeln('ERROR in ', ATest.TestSuiteName, '.', ATest.TestName);
+  writeln('  Exception: ', AError.ExceptionClassName);
+  writeln('  Message:   ', AError.ExceptionMessage);
+  if AError.SourceUnitName <> '' then
+    writeln('  Unit:      ', AError.SourceUnitName);
+  if AError.FailedMethodName <> '' then
+    writeln('  Method:    ', AError.FailedMethodName);
+  if AError.LineNumber <> 0 then
+    writeln('  Line:      ', AError.LineNumber);
+end;
+
+procedure TCustomTestWriter.StartTest(ATest: TTest);
+begin
+  FCurrentTest := ATest;
+  FTestFailed := False; 
+  writeln('Running ', ATest.TestSuiteName, '.', ATest.TestName, '... ');
+end;
+
+procedure TCustomTestWriter.EndTest(ATest: TTest);
+begin
+  if not FTestFailed then  
+    writeln('PASSED');
+  FCurrentTest := nil;
+end;
+
+procedure TCustomTestWriter.StartTestSuite(ATestSuite: TTestSuite);
+begin
+  // Optional: Add any test suite start text
+end;
+
+procedure TCustomTestWriter.EndTestSuite(ATestSuite: TTestSuite);
+begin
+  // Optional: Add any test suite end text
+end;
 
 procedure RegisterTestGroup(const groupName: string);
 begin
@@ -71,6 +244,7 @@ end;
 begin
   groupName := '';
   testName := '';
+  testSuite := GetTestRegistry; // Initialize testSuite
   
   for i := 1 to ParamCount do
   begin
@@ -93,17 +267,10 @@ begin
   RegisterTestGroup(groupName);
   
   testResult := TTestResult.Create;
-  FXMLResultsWriter := TXMLResultsWriter.Create;
-  FDigestResultsWriter := TDigestResultsWriter.Create(nil);
-  testSuite := GetTestRegistry;
+  FCustomWriter := TCustomTestWriter.Create; 
   
   try
-    testResult.AddListener(FXMLResultsWriter);
-    testResult.AddListener(FDigestResultsWriter);
-    FDigestResultsWriter.Category := 'DB';
-    FDigestResultsWriter.RelSrcDir := 'fcl-db';
-
-    FXMLResultsWriter.WriteHeader;
+    testResult.AddListener(FCustomWriter);  
     
     if testName <> '' then
     begin
@@ -128,10 +295,9 @@ begin
         WriteLn('Error: Test group "', groupName, '" not found');
     end;
     
-    FXMLResultsWriter.WriteResult(testResult);
+    FCustomWriter.WriteResult(testResult);  
   finally
     testResult.Free;
-    FXMLResultsWriter.Free;
-    FDigestResultsWriter.Free;
+    FCustomWriter.Free;
   end;
 end.
